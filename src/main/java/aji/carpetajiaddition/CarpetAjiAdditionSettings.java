096d6da6cee20659973b366ae24a631537697bfc
aji <2442733838@qq.com>
2026-04-08 23:44:36 +0800

    添加规则必须设为默认值功能

--- src/main/java/aji/carpetajiaddition/CarpetAjiAdditionSettings.java
+++ src/main/java/aji/carpetajiaddition/CarpetAjiAdditionSettings.java
@@ -1,12 +1,17 @@
 package aji.carpetajiaddition;
 
 import aji.carpetajiaddition.data.DataManager;
+import aji.carpetajiaddition.settings.MustSetDefault;
 import aji.carpetajiaddition.validators.observer.RecipeRuleObserve;
 import carpet.api.settings.Rule;
 import net.fabricmc.loader.api.FabricLoader;
 import net.minecraft.server.MinecraftServer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import java.lang.reflect.Field;
+import java.util.HashSet;
+import java.util.Set;
 
 import static aji.carpetajiaddition.settings.RuleCategory.*;
 import static carpet.api.settings.RuleCategory.*;
@@ -16,6 +21,7 @@ public class CarpetAjiAdditionSettings {
     public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
     public static final String VERSION = FabricLoader.getInstance().getModContainer(MOD_ID).orElseThrow().getMetadata().getVersion().toString();
     public static final CarpetAjiAdditionExtension EXTENSION = new CarpetAjiAdditionExtension();
+    public static final Set<String> MUST_SET_DEFAULT_RULES = new HashSet<>();
     public static MinecraftServer minecraftServer = null;
     public static DataManager data = null;
 
@@ -60,4 +66,12 @@ public class CarpetAjiAdditionSettings {
 
     @Rule(categories = {CAA, RECIPE}, validators = RecipeRuleObserve.class)
     public static boolean dragonBreathRecipe = false;
+
+    static {
+        for (Field field : CarpetAjiAdditionSettings.class.getDeclaredFields()) {
+            if (field.isAnnotationPresent(MustSetDefault.class)){
+                MUST_SET_DEFAULT_RULES.add(field.getName());
+            }
+        }
+    }
 }
--- src/main/java/aji/carpetajiaddition/mixin/carpet/SettingsManagerMixin.java
+++ src/main/java/aji/carpetajiaddition/mixin/carpet/SettingsManagerMixin.java
@@ -2,16 +2,21 @@ package aji.carpetajiaddition.mixin.carpet;
 
 import aji.carpetajiaddition.CarpetAjiAdditionSettings;
 import aji.carpetajiaddition.util.translations.TranslationUtil;
+import carpet.api.settings.CarpetRule;
 import carpet.api.settings.SettingsManager;
 import carpet.utils.Messenger;
 import net.minecraft.commands.CommandSourceStack;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(SettingsManager.class)
 public abstract class SettingsManagerMixin {
+    @Shadow
+    protected abstract int setDefault(CommandSourceStack source, CarpetRule<?> rule, String stringValue);
+
     @Inject(
             method = "listAllSettings",
             at = @At(
@@ -23,5 +28,12 @@ public abstract class SettingsManagerMixin {
     )
     public void listAllSettings(CommandSourceStack source, CallbackInfoReturnable<Integer> cir) {
         Messenger.m(source, "g Carpet Aji Addition " + TranslationUtil.tr("carpetajiaddition.version") + CarpetAjiAdditionSettings.VERSION);
+    }
+
+    @Inject(method = "setRule", at = @At("RETURN"))
+    private void isMustSetDefaultRule(CommandSourceStack source, CarpetRule<?> rule, String value, CallbackInfoReturnable<Integer> cir) {
+        if (CarpetAjiAdditionSettings.MUST_SET_DEFAULT_RULES.contains(rule.name())) {
+            setDefault(source, rule, value);
+        }
     }
 }
